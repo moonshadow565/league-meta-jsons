0be7d69974cfc5730c95ad4f02e8a4aaaa50eec2
moonshadow565 <moonshadow565@hotmail.com>
2020-03-19 22:04:35 +0100

    update

--- auto-update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env sh
-
-while true
-do
-    echo "Updating"
-    ./update.sh
-    sleep 1h
-done
--- update.sh
+++ update.sh
@@ -5,9 +5,9 @@ set -o pipefail
 cd src/manifests
 git fetch
 if [ $(git rev-parse HEAD) == $(git rev-parse @{u}) ]; then
-    echo "No new manifests"
-    exit 0
+    echo "No new jsons"
 fi
+read -p "Press enter key to continue..."
 echo "Updating submodule"
 git pull
 cd ../..
@@ -22,23 +22,30 @@ manifest=$(cat "${realm}/${json}" | grep -Po '(?<=game_patch_url": "https://lol\
 echo "Manifest: ${manifest}"
 if [ -f "${manifest}" ]; then
     echo "Manifest exists"
-    exit 0
 fi
+read -p "Press enter key to continue..."
 echo "Fetching manifest"
 curl -f -o "${manifest}" "https://lol.secure.dyn.riotcdn.net/channels/public/releases/${manifest}"
+read -p "Press enter key to continue..."
 echo "Checking for updates"
 updates=$(../bin/fckrman.exe list "${manifest}" -v -p "League of Legends.exe" | wc -l)
 if [ "${updates}" -lt "1" ]; then
     echo "League not updated"
-    exit 0
 fi
+read -p "Press enter key to continue..."
 echo "Downloading files"
 ../bin/fckrman.exe download "${manifest}" -v -p ".*(dll|exe)" -r 3
-echo "Dumping.."
+read -p "Press enter key to continue..."
+echo "Dumping..."
 rm -rf meta/
 rm -rf Logs/
 cp ../bin/BugSplat.dll BugSplat.dll
 ./League\ of\ Legends.exe
+echo "New files: "
+ls meta/
+echo "Bad entries: "
+grep -F '-1' -r meta/
+read -p "Press enter key to continue..."
 mv meta/* ../live-meta/
 cd ..
 
